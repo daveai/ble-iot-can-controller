35ce79d5c9c7247588cc0371f1f24fb8bf989c1a
Debashis Banerjee <Debashis@Mac-Mini.pyreon.net>
2017-10-09 19:58:44 +0530

    adding picker views for dropdowns

--- CementechThunderbolt/ViewControllers/Ticket/CreateTicketViewController.swift
+++ CementechThunderbolt/ViewControllers/Ticket/CreateTicketViewController.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 
-class CreateTicketViewController: UIViewController {
+class CreateTicketViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var containerView: UIView!
+    var picker = UIPickerView()
+    @IBOutlet weak var state: CustomUITextField!
+    @IBOutlet weak var txtMixDesign: CustomUITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        picker.delegate = self
+        picker.dataSource = self
+        state.inputView = picker
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.plain, target: self, action: #selector(donePicker(sender:)))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: #selector(donePicker(sender:)))
+        
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true    
+        
+        state.inputAccessoryView = toolBar
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,15 +48,25 @@ class CreateTicketViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "Hello"
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    @IBAction func actionStateDropDown(_ sender: Any) {
+        state.becomeFirstResponder()
+    }
+    @objc func donePicker (sender:UIBarButtonItem){
+        print("Hello")
+    }
+    
+    
 }
